78fa559143a8f14eb4b01420408e64bce9d7b4f5
unknown <nikcevici13@gmail.com>
2024-11-18 19:36:54 +0100

    installing websockets

--- src/views/StandbyView.vue
+++ src/views/StandbyView.vue
@@ -1,5 +1,19 @@
-<script setup></script>
-<!-- 2:59:21 -->
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  let echo = new Echo({
+    broadcaster: "pusher",
+    key: "mykey",
+    cluster: "mt1",
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: true,
+    disableStats: true,
+    enabledTransports: ["ws", "wss"],
+  });
+});
+</script>
 <template>
   <div class="pt-16">
     <h1 class="text-3xl font-semibold mb-4">Waiting for ride request...</h1>
